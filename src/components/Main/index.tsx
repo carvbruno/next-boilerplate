27cda6604a046e45e442f4be9f8b0e1ffc4a9dd1
Bruno <bruno.silva@govoll.com>
2022-01-28 11:33:34 -0300

    storybook

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+// import { Container } from './styles';
+
+const Main = () => {
+	return (
+		<>
+			<p>Boilerplate</p>
+		</>
+	)
+}
+
+export default Main
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,18 @@
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+
+import GlobalStyle from 'styles/global'
 
 function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />
+	return (
+		<>
+			<Head>
+				<title>Boilerplate</title>
+			</Head>
+			<GlobalStyle />
+			<Component {...pageProps} />
+		</>
+	)
 }
 
 export default App
